d97cc8dee1ed1f0bbc6b08acc1678d5cad64b989
Bjoern Meyer <bjoern@textcontrol.com>
2019-08-12 16:02:19 +0200

    Initial upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace tx_mailmerge_excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textControl1 = new TXTextControl.TextControl();
+            this.ribbon1 = new TXTextControl.Windows.Forms.Ribbon.Ribbon();
+            this.statusBar1 = new TXTextControl.StatusBar();
+            this.rulerBar1 = new TXTextControl.RulerBar();
+            this.rulerBar2 = new TXTextControl.RulerBar();
+            this.ribbonFormattingTab1 = new TXTextControl.Windows.Forms.Ribbon.RibbonFormattingTab();
+            this.ribbonReportingTab1 = new TXTextControl.Windows.Forms.Ribbon.RibbonReportingTab();
+            this.ribbon1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textControl1
+            // 
+            this.textControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textControl1.Font = new System.Drawing.Font("Arial", 10F);
+            this.textControl1.Location = new System.Drawing.Point(25, 172);
+            this.textControl1.Name = "textControl1";
+            this.textControl1.Ribbon = this.ribbon1;
+            this.textControl1.RulerBar = this.rulerBar2;
+            this.textControl1.Size = new System.Drawing.Size(972, 395);
+            this.textControl1.StatusBar = this.statusBar1;
+            this.textControl1.TabIndex = 1;
+            this.textControl1.UserNames = null;
+            this.textControl1.VerticalRulerBar = this.rulerBar1;
+            // 
+            // ribbon1
+            // 
+            this.ribbon1.Controls.Add(this.ribbonFormattingTab1);
+            this.ribbon1.Controls.Add(this.ribbonReportingTab1);
+            this.ribbon1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ribbon1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.ribbon1.HotTrack = true;
+            this.ribbon1.Location = new System.Drawing.Point(0, 31);
+            this.ribbon1.Name = "ribbon1";
+            this.ribbon1.SelectedIndex = 2;
+            this.ribbon1.Size = new System.Drawing.Size(997, 116);
+            this.ribbon1.TabIndex = 2;
+            this.ribbon1.Text = "ribbon1";
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.statusBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusBar1.Location = new System.Drawing.Point(0, 567);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(997, 22);
+            this.statusBar1.TabIndex = 3;
+            // 
+            // rulerBar1
+            // 
+            this.rulerBar1.Alignment = TXTextControl.RulerBarAlignment.Left;
+            this.rulerBar1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rulerBar1.Location = new System.Drawing.Point(0, 172);
+            this.rulerBar1.Name = "rulerBar1";
+            this.rulerBar1.Size = new System.Drawing.Size(25, 395);
+            this.rulerBar1.TabIndex = 4;
+            this.rulerBar1.Text = "rulerBar1";
+            // 
+            // rulerBar2
+            // 
+            this.rulerBar2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rulerBar2.Location = new System.Drawing.Point(0, 147);
+            this.rulerBar2.Name = "rulerBar2";
+            this.rulerBar2.Size = new System.Drawing.Size(997, 25);
+            this.rulerBar2.TabIndex = 5;
+            this.rulerBar2.Text = "rulerBar2";
+            // 
+            // ribbonFormattingTab1
+            // 
+            this.ribbonFormattingTab1.Location = new System.Drawing.Point(4, 24);
+            this.ribbonFormattingTab1.Name = "ribbonFormattingTab1";
+            this.ribbonFormattingTab1.Size = new System.Drawing.Size(791, 88);
+            this.ribbonFormattingTab1.TabIndex = 1;
+            // 
+            // ribbonReportingTab1
+            // 
+            this.ribbonReportingTab1.Location = new System.Drawing.Point(4, 24);
+            this.ribbonReportingTab1.Name = "ribbonReportingTab1";
+            this.ribbonReportingTab1.Size = new System.Drawing.Size(989, 88);
+            this.ribbonReportingTab1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 589);
+            this.Controls.Add(this.textControl1);
+            this.Controls.Add(this.rulerBar1);
+            this.Controls.Add(this.rulerBar2);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.ribbon1);
+            this.Name = "Form1";
+            this.Text = "MailMerge Sample: Include Excel Files";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ribbon1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TXTextControl.TextControl textControl1;
+        private TXTextControl.Windows.Forms.Ribbon.Ribbon ribbon1;
+        private TXTextControl.Windows.Forms.Ribbon.RibbonFormattingTab ribbonFormattingTab1;
+        private TXTextControl.Windows.Forms.Ribbon.RibbonReportingTab ribbonReportingTab1;
+        private TXTextControl.RulerBar rulerBar2;
+        private TXTextControl.StatusBar statusBar1;
+        private TXTextControl.RulerBar rulerBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tx_mailmerge_excel.Properties;
+using TXTextControl.Windows.Forms.Ribbon;
+using static TXTextControl.Windows.Forms.ResourceProvider;
+
+namespace tx_mailmerge_excel
+{
+    public partial class Form1 : TXTextControl.Windows.Forms.Ribbon.RibbonForm
+    {
+        Report report;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Report
+        {
+            public string Name { get; set; }
+        }
+
+        private void MailMerge_IncludeTextMerging(object sender,
+            TXTextControl.DocumentServer.MailMerge.IncludeTextMergingEventArgs e)
+        {
+            // custom handing of XLSX files
+            switch (Path.GetExtension(e.IncludeTextField.Filename))
+            {
+                case ".xlsx": // Excel file detected
+
+                    if (!File.Exists(e.IncludeTextField.Filename))
+                        return;
+
+                    // load document into temp. ServerTextControl
+                    using (TXTextControl.ServerTextControl tx =
+                        new TXTextControl.ServerTextControl())
+                    {
+                        tx.Create();
+
+                        // Bookmark name is the sheet name of the Excel document
+                        TXTextControl.LoadSettings ls = new TXTextControl.LoadSettings()
+                        {
+                            DocumentPartName = e.IncludeTextField.Bookmark
+                        };
+
+                        try
+                        {
+                            // load the Excel document
+                            tx.Load(
+                                e.IncludeTextField.Filename, 
+                                TXTextControl.StreamType.SpreadsheetML, 
+                                ls);
+                        }
+                        catch {
+                            return;
+                        }
+
+                        byte[] data;
+
+                        // save the document using the TX format
+                        tx.Save(out data, 
+                            TXTextControl.BinaryStreamType.InternalUnicodeFormat);
+
+                        e.IncludeTextDocument = data; // pass back to the field
+                    }
+
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // new ribbon group
+            TXTextControl.Windows.Forms.Ribbon.RibbonGroup rgPreview =
+                new TXTextControl.Windows.Forms.Ribbon.RibbonGroup();
+            rgPreview.SmallIcon = Resources.Preview16;
+            rgPreview.LargeIcon = Resources.Preview32;
+            rgPreview.Text = "Preview Merge";
+            rgPreview.DialogBoxLauncher.Visible = false;
+            rgPreview.HorizontalContentAlignment = TXTextControl.HorizontalAlignment.Center;
+
+            // new ribbon button
+            RibbonButton rbPreview = new RibbonButton();
+            rbPreview.Text = "Preview";
+            rbPreview.LargeIcon = Resources.Preview32;
+            rbPreview.SmallIcon = Resources.Preview16;
+            rbPreview.Click += RbPreview_Click;
+
+            // add button to new gruop
+            rgPreview.RibbonItems.Add(rbPreview);
+            
+            // add group to reporting tab
+            ribbonReportingTab1.RibbonGroups.Insert(0, rgPreview);
+
+            // create a sample data source object
+            report = new Report() { Name = "Test Report" };
+            ribbonReportingTab1.DataSourceManager.LoadSingleObject(report);
+        }
+
+        private void RbPreview_Click(object sender, EventArgs e)
+        {
+            // create a MailMerge instance
+            TXTextControl.DocumentServer.MailMerge mailMerge =
+                new TXTextControl.DocumentServer.MailMerge()
+                {
+                    TextComponent = textControl1
+                };
+
+            // attach the IncludeTextMerging event
+            mailMerge.IncludeTextMerging += MailMerge_IncludeTextMerging;
+
+            // merge template with dummy data
+            mailMerge.MergeObject(report);
+        }
+    }
+}
